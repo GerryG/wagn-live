b58cda6544933617808dbbb6486af127eefd47ba
Gerry Gleason <gerrg@inbox.com>
2014-02-01 10:01:30 -0600

    Fix views for finalized type api/views

--- mods/live/sets/all/rich_html.rb
+++ mods/live/sets/all/rich_html.rb
@@ -3,25 +3,10 @@ format :html do
   view :live_titled, :tags=>:comment do |args|
     wrap :live_titled, args do
       %{
-        #{ _render_header args.merge(:menu_default_hidden=>true, :hidden_type=>true) }
+        #{ _render_header args.merge :hide_menu=>true, :show=>"type #{args[:show]}" }
         #{ wrap_body( :content=>true ) { _render_core args } }
         #{ optional_render :comment_box, args }
       }
     end
-  end
-
-  view :header do |args|
-    %{
-      <h1 class="card-header">
-        #{ args.delete :toggler }
-        #{ _render_title args }
-        #{ _render_type args.merge( :type_class=>"type-hidden" ) if args[:hidden_type] }
-        #{
-          args[:custom_menu] or unless args[:hide_menu]                          # developer config
-            _optional_render :menu, args, (args[:menu_default_hidden] || false)  # wagneer config
-          end
-        }
-      </h1>
-    }
   end
 end
--- mods/live/sets/self/type_select.rb
+++ mods/live/sets/self/type_select.rb
@@ -2,7 +2,9 @@
 format :html do
   view :core do |args|
     %{ <script type="text/template" class="live-type-selection">
-      <span class="live-type-selection">#{ type_field args.merge( :class=>'type-field live-type-field' ) }</span>
+      <span class="live-type-selection">#{
+        type_field args.merge( :no_current_card=>true, :class=>'type-field live-type-field' )
+      }</span>
     </script>}
   end
   view :raw, :core
