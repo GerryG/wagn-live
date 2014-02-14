2ef44f1d5c5227bc9a1aa78c7af3f7f579a94bb1
Gerry Gleason <gerrg@inbox.com>
2014-02-14 07:30:14 -0600

    Simplify json export to build off base :atom view

--- mods/live/sets/all/json.rb
+++ mods/live/sets/all/json.rb
@@ -2,14 +2,12 @@
 format :json do
 
   view :export do |args|
-    h = { :name    => card.name }
-    code = card.codename and h[:code] = code
-    h[:type] = card.type_name
-    h.merge! _render_statu
-    updater = card.updater and h[:updater] = updater.name
-    h[:status] == :real and h[:creator] = card.creator.name
-    h[:content] = card.raw_content
-    h[:value] = _render_core args if @depth < max_depth
+    h = _render_atom.merge _render_statu
+    if h[:status] == :real
+      h[:creator] = card.creator.name
+      h[:updater] = card.updater.name
+      h[:updated_at] = card.updated_at.to_s
+    end
     h
   end
 
